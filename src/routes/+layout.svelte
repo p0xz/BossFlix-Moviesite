b6e3c29fcf04a4539d657061f0c646c47b1e4b5e
p0xz <zeronumb25@gmail.com>
2025-10-23 13:10:25 +0200

    updated few stuff for SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,38 @@
 
 <svelte:head>
 	<link rel="icon" href="/favicon.svg" />
+	<meta name="description" content="Watch your favourite shows and movies free online" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://bossflix.org/" />
+	<meta
+		property="og:title"
+		content="BossFlix - watch your favourite shows and movies free online"
+	/>
+	<meta
+		property="og:description"
+		content="Automate DMs, target the right audience, and grow your main profile on autopilot with DMify."
+	/>
+	<meta property="og:image" content="http://bossflix.org/og-image-bossflix.png" />
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:url" content="http://bossflix.org/" />
+	<meta
+		name="twitter:title"
+		content="BossFlix - watch your favourite shows and movies free online"
+	/>
+	<meta
+		name="twitter:description"
+		content="BossFlix - watch your favourite shows and movies free online"
+	/>
+	<meta name="twitter:image" content="http://bossflix.org/og-image-bossflix.png" />
+
+	<meta
+		name="keywords"
+		content="streaming, movies, tv shows, free, online, watch, bossflix, boss flix, bossflix.org"
+	/>
 </svelte:head>
 
 <main>
---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,29 @@
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async () => {
+	const lastmod = new Date().toISOString();
+
+	const sitemap: string = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset
+            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+
+        <url>
+        <loc>https://bossflix.org/</loc>
+        <lastmod>${lastmod}</lastmod>
+        </url>
+
+
+        </urlset>
+    `.trim();
+
+	return new Response(sitemap, {
+		headers: {
+			'Content-Type': 'application/xml'
+		}
+	});
+};
